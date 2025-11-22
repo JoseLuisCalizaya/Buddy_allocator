d4a7e32f18b5fbccc3f444172a26b18885827f98
JoseLuisCalizaya <joseluisataque@gmail.com>
2025-11-22 01:46:54 -0500

    agregando iertas funciones para la sub-asignacion como es el constructor / destructor y la funcion allocate

---
+++ src/linear.cpp
@@ -0,0 +1,36 @@
+#include "../head/linear.h"
+#include <iostream>
+
+LinearAllocator::LinearAllocator(Buddy_allocation &buddy_ref, size_t pageSize)
+    : buddy(buddy_ref), page_size(pageSize) {}
+
+LinearAllocator::~LinearAllocator() { reset(); }
+
+void *LinearAllocator::allocate(size_t size) {
+  // Pagina existente
+  if (!pages.empty()) {
+    LinearPage &lasPage = pages.back();
+    size_t aligned_size = (size + 3) & ~3;
+
+    if (lasPage.used_offset + aligned_size <= lasPage.total_size) {
+      void *ptr = static_cast<char *>(lasPage.base_ptr) + lasPage.used_offset;
+      lasPage.used_offset += aligned_size;
+      return ptr;
+    }
+  }
+
+  // Pedir una nueva pagina
+  size_t new_page_req = std::max(page_size, size);
+  void *new_block = buddy.malloc(new_page_req);
+  if (!new_block)
+    return nullptr;
+
+  pages.emplace_back(new_block, new_page_req);
+  LinearPage &new_page = pages.back();
+  size_t aligned_size = (size + 3) & ~3;
+  void *ptr = new_page.base_ptr;
+  new_page.used_offset += aligned_size;
+
+  std::cout << "[LinearAllocator] Nueva Pagina creada " << new_page_req << '\n';
+  return ptr;
+}
